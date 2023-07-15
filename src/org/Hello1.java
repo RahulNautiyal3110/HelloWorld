ff6b8d0ef2a9ab1aaa778a0c5ab796658cd5f226
Rahul <Rahulnautiyal3110@gmail.com>
2023-07-15 22:43:58 +0530

    Update Hello1.java

--- src/org/Hello1.java
+++ src/org/Hello1.java
@@ -1,1 +1,16 @@
+public class Hello1 {
 
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		for(int i = 0; i<10 ; i++)
+		{
+			System.out.println("Hello World");
+		}
+		str();
+	}
+	public static void str() {
+		System.out.println("String");
+	}
+
+}
